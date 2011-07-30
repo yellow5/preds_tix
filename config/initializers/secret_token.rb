a64d63429f509ae3d753d26956163d250fd0eee1
Cameron Dykes <yellow5c@gmail.com>
2011-07-30 14:36:13 -0500

    Start empty rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PredsTix::Application.config.secret_token = '6511777b6b05e5a5ca54bedc0c037c9c098d128e53516ab1a4a2591ccc737a002eafcf4aaad8e2ac435a2f151aaeaa55161ffdd163b3e3590b1fdd173a909fb8'
