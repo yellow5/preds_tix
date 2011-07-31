ccc0dca1634bbd1a20e7db830ba845401e73727a
Cameron Dykes <yellow5c@gmail.com>
2011-07-30 23:04:40 -0500

    Remove season column from games/index

--- spec/views/games/index.html.erb_spec.rb
+++ spec/views/games/index.html.erb_spec.rb
@@ -7,12 +7,10 @@ describe "games/index.html.erb" do
     assign(:season, season)
     assign(:games, [
       stub_model(Game,
-        :season_id => 1,
         :opponent => "Opponent",
         :preseason => false
       ),
       stub_model(Game,
-        :season_id => 1,
         :opponent => "Opponent",
         :preseason => false
       )
@@ -26,8 +24,6 @@ describe "games/index.html.erb" do
 
   it "renders a list of games" do
     render
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => 1.to_s, :count => 2
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "tr>td", :text => "Opponent".to_s, :count => 2
     # Run the generator again with the --webrat flag if you want to use webrat matchers
