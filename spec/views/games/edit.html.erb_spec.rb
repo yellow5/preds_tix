39a4fe9c186852a062a8b1e73a10e713c7365e97
Cameron Dykes <yellow5c@gmail.com>
2011-07-30 19:14:20 -0500

    Add seasons selector to Game new/edit

--- spec/views/games/edit.html.erb_spec.rb
+++ spec/views/games/edit.html.erb_spec.rb
@@ -1,12 +1,15 @@
 require 'spec_helper'
 
 describe "games/edit.html.erb" do
+  let!(:season) { Season.create! }
+
   before(:each) do
     @game = assign(:game, stub_model(Game,
       :season_id => 1,
       :opponent => "MyString",
       :preseason => false
     ))
+    assign(:seasons, Season.all)
   end
 
   it "renders the edit game form" do
@@ -14,7 +17,8 @@ describe "games/edit.html.erb" do
 
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "form", :action => games_path(@game), :method => "post" do
-      assert_select "input#game_season_id", :name => "game[season_id]"
+      assert_select "select#game_season_id", :name => "game[season_id]"
+      assert_select "option[value=#{season.id}]"
       assert_select "input#game_opponent", :name => "game[opponent]"
       assert_select "input#game_preseason", :name => "game[preseason]"
     end
--- spec/views/games/new.html.erb_spec.rb
+++ spec/views/games/new.html.erb_spec.rb
@@ -1,12 +1,15 @@
 require 'spec_helper'
 
 describe "games/new.html.erb" do
+  let!(:season) { Season.create! }
+
   before(:each) do
     assign(:game, stub_model(Game,
       :season_id => 1,
       :opponent => "MyString",
       :preseason => false
     ).as_new_record)
+    assign(:seasons, Season.all)
   end
 
   it "renders new game form" do
@@ -14,7 +17,8 @@ describe "games/new.html.erb" do
 
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "form", :action => games_path, :method => "post" do
-      assert_select "input#game_season_id", :name => "game[season_id]"
+      assert_select "select#game_season_id", :name => "game[season_id]"
+      assert_select "option[value=#{season.id}]"
       assert_select "input#game_opponent", :name => "game[opponent]"
       assert_select "input#game_preseason", :name => "game[preseason]"
     end
