42522484bc2390837f7330ef98ca6ab02e456239
Cameron Dykes <yellow5c@gmail.com>
2011-07-30 23:39:01 -0500

    Change puck_drop format, show Y for preseason games, and remove destroy link

--- spec/views/games/index.html.erb_spec.rb
+++ spec/views/games/index.html.erb_spec.rb
@@ -2,19 +2,28 @@ require 'spec_helper'
 
 describe "games/index.html.erb" do
   let(:season) { Season.create!(:years => '2011-12') }
+  let(:puck_drop1) { 1.month.from_now }
+  let(:puck_drop2) { 2.months.from_now }
+  let(:game1) do
+    Game.create!(
+      :season_id => season.id,
+      :opponent  => 'Calgary Flames',
+      :puck_drop => puck_drop1,
+      :preseason => true
+    )
+  end
+  let(:game2) do
+    Game.create!(
+      :season_id => season.id,
+      :opponent  => 'Boston Bruins',
+      :puck_drop => puck_drop2,
+      :preseason => false
+    )
+  end
 
   before(:each) do
     assign(:season, season)
-    assign(:games, [
-      stub_model(Game,
-        :opponent => "Opponent",
-        :preseason => false
-      ),
-      stub_model(Game,
-        :opponent => "Opponent",
-        :preseason => false
-      )
-    ])
+    assign(:games, [game1, game2])
   end
 
   it 'displays season years in header' do
@@ -24,9 +33,10 @@ describe "games/index.html.erb" do
 
   it "renders a list of games" do
     render
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => "Opponent".to_s, :count => 2
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => false.to_s, :count => 2
+    assert_select 'tr>td', :text => puck_drop1.to_s(:pretty), :count => 1
+    assert_select "tr>td", :text => game1.opponent, :count => 1
+    assert_select "tr>td", :text => 'Y', :count => 1
+    assert_select 'tr>td', :text => puck_drop2.to_s(:pretty), :count => 1
+    assert_select "tr>td", :text => game2.opponent, :count => 1
   end
 end
