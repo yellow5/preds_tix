0a588d10716a26b17f3e0603b0596d5e0c2a9926
Cameron Dykes <yellow5c@gmail.com>
2011-07-31 22:11:48 -0500

    Remove full attribute listing and identify games as preseason or regular season

--- spec/views/games/show.html.erb_spec.rb
+++ spec/views/games/show.html.erb_spec.rb
@@ -2,28 +2,46 @@ require 'spec_helper'
 
 describe "games/show.html.erb" do
   let(:puck_drop) { 2.months.from_now }
+  let!(:game) { Game.create!(:puck_drop => puck_drop, :opponent => 'Boston Bruins') }
 
-  before(:each) do
-    @game = assign(:game, stub_model(Game,
-      :season_id => 1,
-      :opponent  => "Opponent",
-      :preseason => false,
-      :puck_drop => puck_drop
-    ))
+  before do
+    assign(:game, game)
   end
 
   it 'displays puck_drop and opponent in header' do
     render
-    rendered.should match("#{@game.puck_drop.to_s(:pretty)} - #{@game.opponent}")
+    rendered.should match("#{game.puck_drop.to_s(:pretty)} - #{game.opponent}")
   end
 
-  it "renders attributes in <p>" do
-    render
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    rendered.should match(/1/)
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    rendered.should match(/Opponent/)
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    rendered.should match(/false/)
+  context 'preaseon game' do
+    before do
+      game.update_attributes!(:preseason => true)
+    end
+
+    it 'identifies the game as preseason' do
+      render
+      rendered.should match('Preseason')
+    end
+
+    it 'does not identify the game as regular season' do
+      render
+      rendered.should_not match('Regular Season')
+    end
+  end
+
+  context 'regular season game' do
+    before do
+      game.update_attributes!(:preseason => false)
+    end
+
+    it 'identifies the game as regular season' do
+      render
+      rendered.should match('Regular Season')
+    end
+
+    it 'does not identify the game as preseason' do
+      render
+      rendered.should_not match('Preseason')
+    end
   end
 end
