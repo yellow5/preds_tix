f58fa7e8c416ca849e878488b8b7045a91807635
Cameron Dykes <yellow5c@gmail.com>
2011-08-13 18:01:04 -0500

    Add ticket form with available ticket holder selector to games/show

--- spec/views/games/show.html.erb_spec.rb
+++ spec/views/games/show.html.erb_spec.rb
@@ -3,9 +3,12 @@ require 'spec_helper'
 describe "games/show.html.erb" do
   let(:puck_drop) { 2.months.from_now }
   let!(:game) { Game.create!(:puck_drop => puck_drop, :opponent => 'Boston Bruins') }
+  let!(:ticket_holder1) { TicketHolder.create!(:name => 'Luke') }
+  let!(:ticket_holder2) { TicketHolder.create!(:name => 'Anakin') }
 
   before do
     assign(:game, game)
+    assign(:ticket_holders, [ticket_holder1, ticket_holder2])
   end
 
   it 'displays puck_drop and opponent in header' do
@@ -42,6 +45,30 @@ describe "games/show.html.erb" do
     it 'does not identify the game as preseason' do
       render
       rendered.should_not match('Preseason')
+    end
+  end
+
+  context 'tickets' do
+    it 'shows a form for each ticket' do
+      render
+      game.tickets.each do |ticket|
+        assert_select 'form', :attributes => { :action => ticket_path(ticket) }
+      end
+    end
+
+    it 'shows game tickets' do
+      render
+      assert_select 'ul>li', /Ticket 1/
+      assert_select 'ul>li', /Ticket 2/
+      assert_select 'ul>li', /Ticket 3/
+      assert_select 'ul>li', /Ticket 4/
+    end
+
+    it 'shows ticket holder options' do
+      render
+      assert_select 'select>option', :text => 'Please select', :count => 4
+      assert_select 'select>option', :text => ticket_holder1.name, :count => 4
+      assert_select 'select>option', :text => ticket_holder2.name, :count => 4
     end
   end
 end
