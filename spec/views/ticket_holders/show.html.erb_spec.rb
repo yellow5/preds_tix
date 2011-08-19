e5005d0fa89533c00fc04320b3c3cb641961de36
Cameron Dykes <yellow5c@gmail.com>
2011-08-18 22:04:34 -0500

    Update ticket_holders/show to display games that ticket holder has tickets to

--- spec/views/ticket_holders/show.html.erb_spec.rb
+++ spec/views/ticket_holders/show.html.erb_spec.rb
@@ -1,18 +1,52 @@
 require 'spec_helper'
 
 describe "ticket_holders/show.html.erb" do
-  before(:each) do
-    @ticket_holder = assign(:ticket_holder, stub_model(TicketHolder,
-      :season_id => 1,
-      :name => "Name"
-    ))
+  let(:season) { Season.create!(:years => '2011-12') }
+  let(:ticket_holder) { TicketHolder.create!(:season_id => season.id, :name => 'Obiwan') }
+  let(:puck_drop1) { 1.month.from_now }
+  let(:puck_drop2) { 2.months.from_now }
+  let(:game1) do
+    Game.create!(
+      :season_id => season.id,
+      :opponent  => 'Calgary Flames',
+      :puck_drop => puck_drop1,
+      :preseason => true
+    ).tap do |game|
+      game.tickets[0].update_attributes!(:ticket_holder_id => ticket_holder.id)
+      game.tickets[1].update_attributes!(:ticket_holder_id => ticket_holder.id)
+    end
+  end
+  let(:game2) do
+    Game.create!(
+      :season_id => season.id,
+      :opponent  => 'Boston Bruins',
+      :puck_drop => puck_drop2,
+      :preseason => false
+    ).tap do |game|
+      game.tickets.each do |ticket|
+        ticket.update_attributes!(:ticket_holder_id => ticket_holder.id)
+      end
+    end
   end
 
-  it "renders attributes in <p>" do
+  before(:each) do
+    assign(:ticket_holder, ticket_holder)
+    assign(:games, [game1, game2])
+  end
+
+  it 'displays name and season years in header' do
     render
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    rendered.should match(/1/)
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    rendered.should match(/Name/)
+    assert_select 'h1', :text => "#{ticket_holder.name}'s Games in #{season.years}"
+  end
+
+  it "renders a list of ticket holder's games" do
+    render
+    assert_select 'tr>td', :text => puck_drop1.to_s(:pretty), :count => 1
+    assert_select 'tr>td', :text => game1.opponent, :count => 1
+    assert_select 'tr>td', :text => 'Y', :count => 1
+    assert_select 'tr>td', :text => '2', :count => 1
+    assert_select 'tr>td', :text => puck_drop2.to_s(:pretty), :count => 1
+    assert_select 'tr>td', :text => game2.opponent, :count => 1
+    assert_select 'tr>td', :text => '4', :count => 1
   end
 end
