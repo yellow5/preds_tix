9b6e74aa0678a7d738193daec47e901d7ec65dc6
Cameron Dykes <yellow5c@gmail.com>
2011-08-19 23:47:13 -0500

    Update layout for ticket views

--- spec/views/tickets/edit.html.haml_spec.rb
+++ spec/views/tickets/edit.html.haml_spec.rb
@@ -1,18 +1,23 @@
 require 'spec_helper'
 
 describe "tickets/edit.html.haml" do
+  let(:game) { Game.create!(:opponent => 'Boston Bruins') }
+  let!(:ticket) { game.tickets.first }
+
   before(:each) do
-    @ticket = assign(:ticket, stub_model(Ticket,
-      :game_id => 1,
-      :ticket_holder_id => 1
-    ))
+    assign(:ticket, ticket)
+  end
+
+  it 'displays opponent in header' do
+    render
+    render.should match("Editing #{ticket.game.opponent} Ticket")
   end
 
   it "renders the edit ticket form" do
     render
 
     # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "form", :action => tickets_path(@ticket), :method => "post" do
+    assert_select "form", :action => tickets_path(ticket), :method => "post" do
       assert_select "input#ticket_game_id", :name => "ticket[game_id]"
       assert_select "input#ticket_ticket_holder_id", :name => "ticket[ticket_holder_id]"
     end
