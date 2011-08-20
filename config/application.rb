1c8020761e00f8b6e7651c27e16687bba70f9a56
Cameron Dykes <yellow5c@gmail.com>
2011-08-20 12:37:24 -0500

    Use minified jquery instead of uncompressed

--- config/application.rb
+++ config/application.rb
@@ -31,7 +31,7 @@ module PredsTix
     # config.i18n.default_locale = :de
 
     # JavaScript files you want as :defaults (application.js is always included).
-    config.action_view.javascript_expansions[:defaults] = %w(jquery jquery_ujs jquery.tablesorter.min jquery-ui-1.8.16.custom.min)
+    config.action_view.javascript_expansions[:defaults] = %w(jquery.min jquery_ujs jquery.tablesorter.min jquery-ui-1.8.16.custom.min)
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
