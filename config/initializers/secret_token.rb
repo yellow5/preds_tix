584a727c13cbf486c4886a27f77cf92c1df3ec02
Cameron Dykes <yellow5c@gmail.com>
2011-08-20 12:44:06 -0500

    Update bundled rails to 3.0.10

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-PredsTix::Application.config.secret_token = '6511777b6b05e5a5ca54bedc0c037c9c098d128e53516ab1a4a2591ccc737a002eafcf4aaad8e2ac435a2f151aaeaa55161ffdd163b3e3590b1fdd173a909fb8'
+PredsTix::Application.config.secret_token = '39383089780d98683b01c145a54b563726da38cda81231b0d45fa7c022e4026712e1ea5b543b66c4ea4b2bd9e67c2e0a39a6639885245b1b3bbea6888b3d0dc8'
