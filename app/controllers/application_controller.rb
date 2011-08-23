734189c4f259a5c8cc1b23c4570303946e2e5ec1
Cameron Dykes <yellow5c@gmail.com>
2011-08-22 20:48:52 -0500

    Use basic authentication for page access in production mode

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  before_filter :authenticate
   before_filter :assign_ticket_holders!
 
   protected
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV['HTTP_USER'] && password == ENV['HTTP_PASS']
+    end if Rails.env.production?
+  end
 
   def assign_ticket_holders!
     @ticket_holders = TicketHolder.all(:order => 'name')
