75fab0225164e79590ef6f0183ba876ee705a86b
Cameron Dykes <yellow5c@gmail.com>
2011-09-05 13:19:49 -0500

    Convert Season specs to shoulda matchers

--- spec/models/season_spec.rb
+++ spec/models/season_spec.rb
@@ -1,51 +1,14 @@
 require 'spec_helper'
 
 describe Season do
-  describe '#years' do
-    it 'is available' do
-      subject.should respond_to(:years)
-    end
-
-    it 'can be assigned' do
-      subject.years.should be_nil
-      subject.years = '2011-2012'
-      subject.years.should == '2011-2012'
-    end
+  describe 'attributes' do
+    it { should have_db_column(:years).of_type(:string) }
+    it { should have_db_column(:created_at).of_type(:datetime) }
+    it { should have_db_column(:updated_at).of_type(:datetime) }
   end
 
   describe 'associations' do
-    context 'games' do
-      let!(:season) { Season.create! }
-      let!(:game1) { Game.create!(:season_id => season.id) }
-      let!(:game2) { Game.create!(:season_id => season.id) }
-      let(:expected_games) { [ game2, game1 ] }
-
-      subject { season }
-
-      it 'is established' do
-        subject.should respond_to(:games)
-      end
-
-      it 'returns related records' do
-        subject.games.should eq(expected_games)
-      end
-    end
-
-    context 'ticket_holders' do
-      let!(:season) { Season.create! }
-      let!(:ticket_holder1) { TicketHolder.create!(:season_id => season.id) }
-      let!(:ticket_holder2) { TicketHolder.create!(:season_id => season.id) }
-      let(:expected_ticket_holders) { [ ticket_holder2, ticket_holder1 ] }
-
-      subject { season }
-
-      it 'is established' do
-        subject.should respond_to(:ticket_holders)
-      end
-
-      it 'returns related records' do
-        subject.ticket_holders.should == expected_ticket_holders
-      end
-    end
+    it { should have_many(:games) }
+    it { should have_many(:ticket_holders) }
   end
 end
