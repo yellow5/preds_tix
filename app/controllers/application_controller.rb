97c0483e2157eddace4c2b5893f1a728fe5f8141
Cameron Dykes <yellow5c@gmail.com>
2012-11-22 08:46:42 -0600

    Add ApplicationController#assign_current_season!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   before_filter :authenticate
+  before_filter :assign_current_season!
   before_filter :assign_ticket_holders!
 
   protected
@@ -10,6 +11,10 @@ class ApplicationController < ActionController::Base
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV['HTTP_USER'] && password == ENV['HTTP_PASS']
     end if Rails.env.production?
+  end
+
+  def assign_current_season!
+    @current_season = Season.order('years desc').first
   end
 
   def assign_ticket_holders!
--- spec/shared_behaviors/application_controller_behavior.rb
+++ spec/shared_behaviors/application_controller_behavior.rb
@@ -1,4 +1,12 @@
 shared_examples_for 'application controller subclass' do
+  describe '#assign_current_season!' do
+    it 'assigns current season by years descending to @current_season' do
+      Season.should_receive(:order).with('years desc').and_return([1, 2])
+      subject.send(:assign_current_season!)
+      assigns(:current_season).should eq 1
+    end
+  end
+
   describe '#assign_ticket_holders!' do
     it 'assigns ticket holders by name to @ticket_holders' do
       TicketHolder.should_receive(:order).with('name').and_return(:mock_results)
