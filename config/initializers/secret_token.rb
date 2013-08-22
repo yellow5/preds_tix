daae15d92a993d7b7f2b617fff12b59e99defa0f
Cameron Dykes <yellow5c@gmail.com>
2013-08-21 23:57:16 -0500

    Update all the things for rails 4

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,12 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-PredsTix::Application.config.secret_token = '7e2f8c3502ca805a6f51d2b5adb9b4155e4a1497fbcaf5875ecdc964fe2f77e60b82f40d49642654386c398733adb27c55303a56ab4f8cf0831634d93d1f6f40'
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PredsTix::Application.config.secret_key_base = 'cd617451b3fb02d6d428e4564d690ed97d9980ebc5248a01693cb14ab09a5c15f0fd488b2b4588a47d31e591616403f4ed5cfef4b48dcc2141d944bb27e1f9b5'
