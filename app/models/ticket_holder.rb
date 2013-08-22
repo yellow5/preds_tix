e5ba994cb7d4f622c2e6e6668c6c204dfbd89582
Cameron Dykes <yellow5c@gmail.com>
2013-08-22 00:22:29 -0500

    Update TicketHolder.games to put conditions into a scope block

--- app/models/ticket_holder.rb
+++ app/models/ticket_holder.rb
@@ -1,7 +1,7 @@
 class TicketHolder < ActiveRecord::Base
   belongs_to :season
   has_many :tickets
-  has_many :games, :through => :tickets, :uniq => true, :order => :puck_drop
+  has_many :games, -> { order(:puck_drop).uniq }, :through => :tickets
 
   def max_tickets_claimed?
     tickets.count >= season.tickets.count / season.ticket_holders.count
